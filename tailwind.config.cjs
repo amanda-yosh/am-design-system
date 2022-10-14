6e9906607ed7ec6eee8e01f480b8ca41b41db7fc
Amanda Maria Costa de Almeida <amandamca.aa@gmail.com>
2022-10-14 12:13:14 -0300

    refactor: colors on tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,15 +13,27 @@ module.exports = {
       '2xl': 32,
     },
     colors: {
-      'pink-100': '#FFF1F2',
-      'pink-200': '#FFE4E6',
-      'pink-300': '#FECDD3',
-      'pink-400': '#FDA4AF',
-      'pink-800': '#9F1239',
-      'pink-900': '#881337',
-      'gray-700': '#7C7C8A',
-      'black-800': '#202024',
-      'black-900': '#121214',
+      transparent: 'transparent',
+      white: {
+        000: '#FFFFFF'
+      },
+      pink: {
+        100: '#FFF1F2',
+        200: '#FFE4E6',
+        300: '#FECDD3',
+        400: '#FDA4AF',
+        700: '#BE123C',
+        800: '#9F1239',
+        900: '#881337',
+      },
+      gray: {
+        700: '#7C7C8A'
+      },
+      black: {
+        000: '#000000',
+        800: '#202024',
+        900: '#121214',
+      },
     },
     extend: {
       fontFamily: {
