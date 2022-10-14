564d9fb628fb3e778cf472c59b68ecc9a42f4e5f
Amanda Maria Costa de Almeida <amandamca.aa@gmail.com>
2022-10-14 10:25:51 -0300

    feat: create Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,19 @@
+import clsx from 'clsx';
+import { ReactNode } from 'react';
+
+export interface ButtonProps {
+  children: ReactNode,
+}
+
+export function Button ({ children }: ButtonProps) {
+
+  return (
+    <button className={clsx(
+      'py-4 px-3 rounded-lg font-semibold text-sm w-full',
+      'bg-pink-200 text-pink-900 transition-colors hover:bg-pink-400',
+      'active:bg-pink-800 active:text-pink-400'
+    )}>
+      { children }
+    </button>
+  )
+}
