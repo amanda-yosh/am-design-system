7a7278555264626525f2ca50dadc0d0008ba7df6
Amanda Maria Costa de Almeida <amandamca.aa@gmail.com>
2022-10-14 08:51:42 -0300

    chore: create tokens

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,25 @@ module.exports = {
     './src/**/*.tsx',
   ],
   theme: {
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      '2xl': 32,
+    },
+    colors: {
+      'pink-100': '#FFF1F2',
+      'pink-200': '#FFE4E6',
+      'pink-300': '#FECDD3',
+      'pink-400': '#FDA4AF',
+      'pink-800': '#9F1239',
+      'pink-900': '#881337',
+      'gray-700': '#7C7C8A',
+      'black-800': '#202024',
+      'black-900': '#121214',
+    },
     extend: {
       fontFamily: {
         sans: 'Raleway, sans-serif',
